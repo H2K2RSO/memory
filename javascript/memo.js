551b34022171ae5455ac7897e10278936226a96e
Hind KAROUACH <hind.karouach2023@campus-eni.fr>
2023-11-14 16:32:32 +0100

    Création des pages html et css fonctionelles et ajout des premières images

---
+++ javascript/memo.js
@@ -0,0 +1,34 @@
+//exemple :  import { getData, saveData } from "./save.js";
+
+// ----------------------------------------------
+// ---------------- formulaires : ---------------
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// ------------------ jeu : ---------------------
+
+// function changementImg(event) {
+//     $(event.target).attr("scr", "images/dinos")
+// }
+
+
+
+
+
+// restart (){
+
+// }
+
+// document.body.addEventListener("keydown", (event) => {
+//   if (KeyboardEvent.key === 32) {
+//     this.restart();
+//   }
+// });
---
+++ javascript/save.js
@@ -0,0 +1,3 @@
+export {
+	nom2Fonction
+}
